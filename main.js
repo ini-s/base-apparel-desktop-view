347b2c645b5fccd5aff0cbade1ca83fcafd4feb0
IMS <inisoetan2014@gmail.com>
2023-01-25 14:44:25 +0000

    fix: change the layout size

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const message = document.querySelector(".error-message")
 const icon = document.querySelector(".error-icon")
 const input = document.querySelector("input")
 const btn = document.querySelector(".submit-btn")
-const inputText = document.querySelector("input").value
+const inputText = document.querySelector("input")
 
 btn.addEventListener("click", () => {
+    console.log(inputText.value)
     const mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/
-    if (inputText.match(mailFormat)) {
+    if (inputText.value.match(mailFormat)) {
         console.log("successful")
     }
     else {
@@ -15,5 +16,3 @@ btn.addEventListener("click", () => {
         input.classList.add("active")
     }
 })
-
-console.log(inputText)
